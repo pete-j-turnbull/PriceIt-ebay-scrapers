6f5ed98092535cd5521615d9095d70da89494df5
Pete Turnbull <pete.j.turnbull@gmail.com>
2015-11-24 13:08:29 +0000

    Removed 'Not specified' option for all lists and ordered option lists by popularity

--- getFeatures.js
+++ getFeatures.js
@@ -17,7 +17,6 @@ var handleSpecialFeatures = function(featureName) {
 };
 var constructFeaturesUrl = function (searchTerm) {
 	var searchTermString = encodeURIComponent(searchTerm).replace('%20', '+');
-	log.debug(searchTermString);
 	return 'http://www.ebay.co.uk/sch/i.html?LH_Auction=1&_nkw=' + searchTermString + '&LH_PrefLoc=1&LH_Complete=1&LH_Sold=1';
 };
 var constructOptionsUrl = function (searchTerm, featureName) {
@@ -32,6 +31,7 @@ var _getOptions = async (function (searchTerm, featureName) {
 	var html = await (scrape.scrape(constructOptionsUrl(searchTerm, featureName)));
 	var options = _(JSON.parse(html).values)
 		.map(function (opt) { return {title: opt.title, count: opt.count}; })
+		.sort((o1, o2) => o2.count - o1.count)
 		.value();
 	return options;
 });
@@ -41,16 +41,19 @@ var _getFeatures = async (function (params) {
 	var html = await (scrape.scrape(constructFeaturesUrl(searchTerm)));
 	var $ = cheerio.load(html);
 
+	//Feature names
 	var featureNames = _($("span[class='pnl-h']"))
 		.map(item => _.trim($(item).text()))
 		.without('Format', 'Seller', 'Item location', 'Delivery options', 'Show only', 'Price', 'Language')
 		.value();
-	log.debug(featureNames);
+	//Option lists
 	var optionLists = await (Promise.map(featureNames, fName => _getOptions(searchTerm, fName)));
 
+	//Score features
 	var featureScores = [];
 	optionLists.forEach(function (optList) {
 		// Use this to work out if a feature is of any use...
+		
 		var notSpecifiedCount = 0;
 		var totalCount = 0;
 		optList.forEach(function (opt) {
@@ -63,6 +66,21 @@ var _getFeatures = async (function (params) {
 		featureScores.push(score);
 	});
 
+	//Remove 'Not specified' from optionLists
+	optionLists = _(optionLists)
+		.map(function(optList) {
+			nOptList = [];
+			optList.forEach(function(opt) {
+				if (opt.title != 'Not specified') {
+					nOptList.push(opt);
+				}
+			});
+			return nOptList.slice(0, 4);
+		})
+		.value();
+
+
+	// Create features object
 	var features = [];
 	for (i = 0; i < featureNames.length; i++) {
 		var optList = _(optionLists[i])
@@ -74,14 +92,15 @@ var _getFeatures = async (function (params) {
 	log.debug(features);
 
 	featuresObj = {};
-	features.slice(0, 4).forEach(function(feature) {
+	features.slice(0, 8).forEach(function(feature) {
 		var key = feature.name;
 		var opts = feature.options;
 		featuresObj[key] = {options: opts};
 	});
 	return {features: featuresObj};
-
 });
+
+
 module.exports.getFeatures = async (function (params) {
 	// Run a request to eBay with the correct searchItem
 	// Pull out features and choose only popular ones
@@ -100,3 +119,4 @@ module.exports.getFeatures = async (function (params) {
 	*/
 	return await (_getFeatures(params))
 });
+
